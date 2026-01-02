512359433d1cc1040a805ebb6529295d392c237f
MANJUNATH GOUDA <153203458+13Manjunath@users.noreply.github.com>
2026-01-02 12:06:53 +0530

    Update preproces.py

--- Resume_ScreeningAI/src/preproces.py
+++ Resume_ScreeningAI/src/preproces.py
@@ -1,1 +1,25 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+
+# Download stopwords once
+nltk.download('stopwords')
+
+stop_words = set(stopwords.words("english"))
+
+def clean_text(text):
+    # Remove special characters and numbers
+    text = re.sub(r'[^a-zA-Z ]', ' ', text)
+
+    # Convert multiple spaces to single space
+    text = re.sub(r'\s+', ' ', text)
+
+    # Lowercase
+    text = text.lower()
+
+    # Remove stopwords
+    tokens = text.split()
+    tokens = [word for word in tokens if word not in stop_words]
+
+    return " ".join(tokens)
 
