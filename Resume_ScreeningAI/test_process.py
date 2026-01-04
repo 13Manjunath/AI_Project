768cc14438a4dc15fd9306c0276851bfd3e1eb18
MANJUNATH GOUDA <153203458+13Manjunath@users.noreply.github.com>
2026-01-04 16:29:15 +0530

    Create test_process.py

---
+++ Resume_ScreeningAI/test_process.py
@@ -0,0 +1,10 @@
+from src.parser import extract_text
+from src.skill_extractor import extract_skills
+
+# Replace with your resume file name
+resume_text = extract_text("data/resume/Manjunath_2025_Rolls.pdf")
+
+skills = extract_skills(resume_text)
+
+print("Skills found in resume:")
+print(skills)
