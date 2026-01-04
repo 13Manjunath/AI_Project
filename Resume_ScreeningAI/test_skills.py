f5bf7f53474936ed08149c238c51016466d7aaec
MANJUNATH GOUDA <153203458+13Manjunath@users.noreply.github.com>
2026-01-04 16:30:11 +0530

    Update test_skills.py

--- Resume_ScreeningAI/test_skills.py
+++ Resume_ScreeningAI/test_skills.py
@@ -1,1 +1,11 @@
+from src.parser import extract_text
+from src.skill_extractor import extract_skills
+
+# Replace with your resume file name
+resume_text = extract_text("data/resume/Manjunath_2025_Rolls.pdf")
+
+skills = extract_skills(resume_text)
+
+print("Skills found in resume:")
+print(skills)
 
